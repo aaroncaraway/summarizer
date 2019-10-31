2cda6a35369c2e938e42f6bdb7394f68545d58b1
aaroncaraway <aaroncaraway42@gmail.com>
2019-10-31 10:42:04 -0700

    Add result page

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ app = Flask(__name__)
 
 def run_the_thing(input):
     return input + ' loves cats'
-    
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
         # get `content` from form
         task_content = request.form['content']
         gobs_program = run_the_thing(task_content)
-        return gobs_program
+        return render_template('result.html', passed=gobs_program)
     else:
         return render_template('index.html')
 
