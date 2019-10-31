6745e3af4de830894054d7e11d392fcb8e897408
aaroncaraway <aaroncaraway42@gmail.com>
2019-10-31 11:04:48 -0700

    Getting the first 5 sentences

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, url_for, request
 from bs4 import BeautifulSoup
 import urllib.request
+import nltk
+import re
 
 app = Flask(__name__)
 
@@ -8,7 +10,16 @@ def run_the_thing(input):
     scraped_data = urllib.request.urlopen(input)
     article = scraped_data.read()
     parsed_article = BeautifulSoup(article)
-    return parsed_article
+    paragraphs = parsed_article.find_all('p')
+    article_text = ""
+    for p in paragraphs:
+        article_text += p.text
+    article_text = re.sub(r'\[[0-9]*\]', '', article_text)
+    article_text = re.sub(r'\s+', ' ', article_text)
+    formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text)
+    formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
+    sentence_list = nltk.sent_tokenize(article_text)
+    return sentence_list[:5]
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
