cf407ecb8dc09fc1c459a85d5440375558ded38c
aaroncaraway <aaroncaraway42@gmail.com>
2019-10-31 10:59:01 -0700

    Getting soup

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template, url_for, request
+from bs4 import BeautifulSoup
+import urllib.request
 
 app = Flask(__name__)
 
 def run_the_thing(input):
-    return input + ' loves cats'
+    scraped_data = urllib.request.urlopen(input)
+    article = scraped_data.read()
+    parsed_article = BeautifulSoup(article)
+    return parsed_article
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -14,6 +19,23 @@ def index():
         return render_template('result.html', passed=gobs_program)
     else:
         return render_template('index.html')
+
+@app.route('/summary/<int:id>', methods=['GET', 'POST'])
+def summary(id):
+    task = Todo.query.get_or_404(id)
+    
+    if request.method == 'POST':
+        task.content = request.form['content']
+        try:
+            # db.session.update(task_to_update)
+            # commit() does the update for us don't need ^^
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was a problem updating that task'
+    else:
+        return render_template('update.html', task=task)
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
